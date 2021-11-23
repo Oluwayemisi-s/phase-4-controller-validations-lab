23c42b135e73676012fe04ef4f1ca989e9f1ffaf
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-11-23 08:31:29 -0800

    commit tree 0a661ee03afeb877a2a454fa24f5cf6482d7eec8

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,8 @@
 class AuthorsController < ApplicationController
-  
+
+  #Change AuthorsController#create to send back the error messages if the new author is invalid
+  #update to send back the error messages if the updated post is invalid
+
   def show
     author = Author.find(params[:id])
 
@@ -7,9 +10,10 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
